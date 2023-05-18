4fdba95f2df8aea3807db60cb9b15262a260ab75
Thein Naing <theinnaing0908@gmail.com>
2023-05-18 12:34:27 +0800

    update useEffect again

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies(movies);
-  }, [searchTerm]);
+    searchMovies(searchTerm);
+  }, []);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
