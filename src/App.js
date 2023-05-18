d57f0fca6c61501e84350968cd9345c91c1c77f5
Thein Naing <theinnaing0908@gmail.com>
2023-05-18 12:44:44 +0800

    update useEffect function again

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies(searchTerm);
+    searchMovies("wonder woman");
   });
 
   const searchMovies = async (title) => {
